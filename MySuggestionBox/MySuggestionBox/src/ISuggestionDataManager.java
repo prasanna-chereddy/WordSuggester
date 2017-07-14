2d50606cc720d9987aeb91081be29af152e09e2d
LAKSHMI PRASANNA CHEREDDY <prasannachereddy16@gmail.com>
2017-07-14 22:24:43 +0530

    Update ISuggestionDataManager.java

--- MySuggestionBox/MySuggestionBox/src/ISuggestionDataManager.java
+++ MySuggestionBox/MySuggestionBox/src/ISuggestionDataManager.java
@@ -1,16 +1,6 @@
 
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author GEETHA
- */
 public interface ISuggestionDataManager {
     ArrayList<String> getSuggestions(String prefix);
 }
